c45206eb78248e5d21ef4bfc271503819306850d
Léo Piérot <leo.fpierot@gmail.com>
2018-03-28 14:12:19 +0200

    bb optim : plus de raycast à chaque frame. Corrections au niveau du comportement du saut.

--- Assets/Controls/Script/Player.cs
+++ Assets/Controls/Script/Player.cs
@@ -16,8 +16,11 @@ public class Player : MonoBehaviour
     private Vector3     m_VerticalDirection = Vector3.zero;
     private float       m_DistToGround = 0f;
     private float       m_DistToSide = 0f;
+    private bool        m_ShouldBeDragged = false;
     private bool        m_IsGrippingWall = false;
     private bool        m_IsDraggedDown = false;
+    private bool        m_IsDashing = false;
+    private int         m_AvailableJumps = 1;
 
     private void Start()
     {
@@ -29,7 +32,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(IsInAir() && !m_IsDraggedDown)
+        if(m_Rigidbody.velocity.y != 0 && !m_IsDraggedDown)
         {
             StartCoroutine(DragDownCoroutine());
         }
@@ -37,17 +40,21 @@ public class Player : MonoBehaviour
 
     public void MoveHorizontal(float iInputValue)
     {
-        //on ne se déplace que si on n'est pas collé à un mur.
-        m_HorizontalDirection = iInputValue * Vector3.right * Time.deltaTime;
-        if (!HasGripOnWall(m_HorizontalDirection))
+        if(!m_IsDashing)
         {
-            transform.Translate(m_HorizontalDirection * speed);
+            //on ne se déplace que si on n'est pas collé à un mur.
+            m_HorizontalDirection = iInputValue * Vector3.right * Time.deltaTime;
+            if (!HasGripOnWall(m_HorizontalDirection))
+            {
+                transform.Translate(m_HorizontalDirection * speed);
+            }
+            //sinon, on s'y accroche.
+            else if (!m_IsGrippingWall)
+            {
+                StartCoroutine(GrippingWallCoroutine(Vector3.Normalize(m_HorizontalDirection)));
+            }
         }
-        //sinon, on s'y accroche.
-        else if (!m_IsGrippingWall)
-        {
-            StartCoroutine(GrippingWallCoroutine(Vector3.Normalize(m_HorizontalDirection)));
-        }
+        
             
     }
 
@@ -55,6 +62,10 @@ public class Player : MonoBehaviour
     {
         //m_VerticalDirection = iInputValue * transform.right * Time.deltaTime;
         //transform.Translate(m_VerticalDirection * speed);
+        if (!m_IsDashing && iInputValue != 0)
+        {
+            StartCoroutine(DashCoroutine());
+        }
     }
 
     public void Fire()
@@ -64,10 +75,54 @@ public class Player : MonoBehaviour
 
     public void Jump()
     {
-        if(IsGrounded() || m_IsGrippingWall)
+        if (!m_IsDashing)
         {
-            Vector3 direction = m_VerticalDirection + m_HorizontalDirection + Vector3.up;
-            m_Rigidbody.velocity = (direction * jumpMultiplier);
+            //if (IsGrounded() || m_IsGrippingWall)
+            if (m_AvailableJumps > 0)
+            {
+                Vector3 direction = m_VerticalDirection + m_HorizontalDirection + Vector3.up;
+                m_Rigidbody.velocity = (direction * jumpMultiplier);
+                SpendJump();
+            }
+        }
+    }
+
+    public void Dash()
+    {
+        
+    }
+
+    private void SpendJump()
+    {
+        m_AvailableJumps = (int)Mathf.Clamp01(m_AvailableJumps - 1);
+    }
+
+    private void GiveJump()
+    {
+        m_AvailableJumps = (int)Mathf.Clamp01(m_AvailableJumps + 1);
+    }
+
+    private IEnumerator DashCoroutine()
+    {
+        m_IsDashing = true;
+        m_Rigidbody.isKinematic = true;
+        GiveJump();
+
+        yield return MoveToPosition(transform.position + transform.right, .1f);
+
+        m_IsDashing = false;
+        m_Rigidbody.isKinematic = false;
+    }
+
+    private IEnumerator MoveToPosition(Vector3 iNewPos, float iTime)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = transform.position;
+        while (elapsedTime < iTime)
+        {
+            transform.position = Vector3.Lerp(startingPos, iNewPos, (elapsedTime / iTime));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
         }
     }
 
@@ -95,7 +150,8 @@ public class Player : MonoBehaviour
     {
         m_IsDraggedDown = true;
         Vector3 dragVector = Vector3.zero;
-        while (IsInAir())
+
+        while (m_ShouldBeDragged)
         {
             yield return new WaitForEndOfFrame();
             //tweak de l'accélération en chute
@@ -110,9 +166,8 @@ public class Player : MonoBehaviour
             }
             m_Rigidbody.velocity += dragVector;
         }
-        m_IsDraggedDown = false;
 
-        //ici, le joueur touche le sol ou un mur.
+        m_IsDraggedDown = false;
     }
 
     private IEnumerator GrippingWallCoroutine(Vector3 iGrippingDirection)
@@ -120,6 +175,7 @@ public class Player : MonoBehaviour
         m_IsGrippingWall = true;
         m_Rigidbody.velocity = Vector3.zero;
         m_Rigidbody.isKinematic = true;
+
         //tant que le direction dans laquelle on se déplace va dans le même sens la direction dans laquelle on s'est aggripé,
         //on coupe le joueur du moteur physique.
         while (Vector3.Dot(iGrippingDirection, m_HorizontalDirection) > 0)
@@ -128,9 +184,36 @@ public class Player : MonoBehaviour
         }
 
         m_Rigidbody.isKinematic = false;
+        m_ShouldBeDragged = false;
         //Pour laisser un peu de temps au joueur de sauter après avoir lâché un mur.
         yield return new WaitForSeconds(.2f);
+        m_ShouldBeDragged = true;
         m_IsGrippingWall = false;
         yield return StartCoroutine(DragDownCoroutine());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("SolidEnvironment"))
+        {
+            m_ShouldBeDragged = false;
+            GiveJump();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("SolidEnvironment"))
+        {
+            m_ShouldBeDragged = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("SolidEnvironment"))
+        {
+            m_ShouldBeDragged = true;
+        }
     }
 }
