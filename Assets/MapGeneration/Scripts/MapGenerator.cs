4ec232938aae6615b7e0105904d8e4807056b70f
Nathan Mugnier <nathan.mugnier@wanadoo.fr>
2018-03-29 13:16:09 +0200

    A* implementation, verticallity in map gen

--- Assets/MapGeneration/Scripts/MapGenerator.cs
+++ Assets/MapGeneration/Scripts/MapGenerator.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+struct node {
+    public int x;
+    public int y;
+    public int cost;
+    public float heuristique;
+};
+
 public class MapGenerator : MonoBehaviour
 {
     public GameObject debugTile = null;
@@ -17,6 +25,7 @@ public class MapGenerator : MonoBehaviour
 	public int maxLevelGap = 7;
 	public int maxGroundLevels = 5;
     public int MIN_SOLID_BLOCK_PER_MAP_INTERIOR = 12;
+    public float solidOverPathable = 0.03f;
     const int NUMBER_OF_PLAYER = 4;
     public GameObject[] Players = new GameObject[4];
     int[][] cellValues = null;
@@ -126,18 +135,189 @@ public class MapGenerator : MonoBehaviour
     }
 
 	void DetermineSolidity(){
-        while (!IsValidMap()){
+        do
+        {
             ReinitialiseMatrix();
             GenerateSolidityMap();
-        }
-	}
+        } while (!IsValidMap());
 
-    bool IsValidMap(){
+    }
+
+    bool IsValidMap()
+    {
+        //IsPathBetweenTwoPoints(new int[2] { 1, 1 }, new int[2] { NUMBER_OF_ROW - 2, 1 });
+        //IsPathToHole();
         return (
             IsEnoughtBlocksInMap() &&
             IsNoBlockingLayer() &&
-            IsNotCollidingWithPlayer()
+            IsNotCollidingWithPlayer() &&
+            IsPathToHole() &&
+            IsPathBetweenPlayers()
             );
+    }
+
+    bool IsPathBetweenPlayers(){
+        return true;
+    }
+
+    bool IsPathToHole()
+    {
+        for (int i = 0; i < NUMBER_OF_COLUMN; i++)
+        {
+            if (cellValues[NUMBER_OF_ROW - 1][i] == 0)
+            {
+                if (IsPathBetweenTwoPoints(new int[2] { 1, 1 }, new int[2] { NUMBER_OF_ROW - 1, i }))
+                {
+                    Debug.Log("RETURN TRUE");
+                    return true;
+                }
+            }
+        }
+        Debug.Log("RETURN FALSe");
+        return false;
+    }
+
+    bool IsPathBetweenTwoPoints(int[] start, int[] end){
+        //@params : Coordinate of start (i, j) and coordinate of end (i, j)
+        List<node> closedList = new List<node>();
+        List<node> openedList = new List<node>();
+        node startNode = new node();
+        node endNode = new node();
+        node currentNode;
+        node closeNode;
+        int i = 0;
+        int lastNodePosition = 0;
+        int k = 0;
+        startNode.x = start[0];
+        startNode.y = start[1];
+        startNode.cost = 0;
+        startNode.heuristique = 0;
+        endNode.x = end[0];
+        endNode.y = end[1];
+        endNode.cost = 1;
+        endNode.heuristique = 0;
+        openedList.Add(startNode);
+        while (openedList.Count != 0)
+        {
+            currentNode = openedList[0];
+            lastNodePosition = 0;   
+            for (i = 1; i < openedList.Count; i++)
+            {
+                if (CompareTwoNodes(openedList[i], currentNode) == 1){
+                    currentNode = openedList[i];
+                    lastNodePosition = i;
+                }
+            }
+            //DebugPrintList(openedList);
+            openedList.RemoveAt(lastNodePosition);
+            //Debug.Log("Remove currentNode from list " + lastNodePosition + " SIZEOF(openedList)" + openedList.Count);
+            if (currentNode.x == endNode.x && currentNode.y == endNode.y)
+            {
+                //Debug.Log("Found");
+                return true;
+            }
+            //Debug.Log("Coordonnées currentNode" + currentNode.x + " " + currentNode.y);
+            if (cellValues[currentNode.x][currentNode.y-1] != 1)
+            {
+                closeNode.x = currentNode.x;
+                closeNode.y = currentNode.y - 1;
+                closeNode.cost = currentNode.cost + 1;
+                closeNode.heuristique = closeNode.cost + Vector2.Distance(new Vector2(closeNode.x, closeNode.y), new Vector2(endNode.x, endNode.y));
+                if(!NodeInListAndCheaper(closedList, closeNode) && !NodeInListAndCheaper(openedList, closeNode))
+                {
+                    //Debug.Log("Add to openedList" + closeNode.x + " " + closeNode.y + " " + closeNode.cost);
+                    openedList.Add(closeNode);
+                }
+            }
+            if (cellValues[currentNode.x][currentNode.y+1] != 1)
+            {
+                closeNode.x = currentNode.x;
+                closeNode.y = currentNode.y + 1;
+                closeNode.cost = currentNode.cost + 1;
+                closeNode.heuristique = closeNode.cost + Vector2.Distance(new Vector2(closeNode.x, closeNode.y), new Vector2(endNode.x, endNode.y));
+                if (!NodeInListAndCheaper(closedList, closeNode) && !NodeInListAndCheaper(openedList, closeNode))
+                {
+                    //Debug.Log("Add to openedList" + closeNode.x + " " + closeNode.y);
+                    openedList.Add(closeNode);
+                }
+            }
+            if (cellValues[currentNode.x-1][currentNode.y] != 1)
+            {
+                closeNode.x = currentNode.x-1;
+                closeNode.y = currentNode.y;
+                closeNode.cost = currentNode.cost + 1;
+                closeNode.heuristique = closeNode.cost + Vector2.Distance(new Vector2(closeNode.x, closeNode.y), new Vector2(endNode.x, endNode.y));
+                if (!NodeInListAndCheaper(closedList, closeNode) && !NodeInListAndCheaper(openedList, closeNode))
+                {
+                    //Debug.Log("Add to openedList" + closeNode.x + " " + closeNode.y);
+                    openedList.Add(closeNode);
+                }
+            }
+            if (currentNode.x < NUMBER_OF_ROW-1) {
+                if (cellValues[currentNode.x + 1][currentNode.y] != 1)
+                {
+                    closeNode.x = currentNode.x + 1;
+                    closeNode.y = currentNode.y;
+                    closeNode.cost = currentNode.cost + 1;
+                    closeNode.heuristique = closeNode.cost + Vector2.Distance(new Vector2(closeNode.x, closeNode.y), new Vector2(endNode.x, endNode.y));
+                    if (!NodeInListAndCheaper(closedList, closeNode) && !NodeInListAndCheaper(openedList, closeNode))
+                    {
+                        //Debug.Log("Add to openedList" + closeNode.x + " " + closeNode.y);
+                        openedList.Add(closeNode);
+                    }
+                }
+            }
+            //Debug.Log("SIZEOF(openedList)");
+            //Debug.Log(openedList.Count);
+            closedList.Add(currentNode);
+        }
+        return false;
+    }
+
+    void DebugPrintList(List<node> list)
+    {
+        string s = "";
+        for(int i = 0; i < list.Count; i++)
+        {
+            s += "(" + list[i].x + " " + list[i].y + "," + list[i].heuristique + ")";
+        }
+        Debug.Log(s);
+    }
+
+    bool NodeInListAndCheaper(List<node> list, node toFind)
+    {
+        if (toFind.x >= NUMBER_OF_ROW || toFind.x <= 0)
+        {
+            return true;
+        }
+        if (toFind.y >= NUMBER_OF_COLUMN-1 || toFind.y <= 0)
+        {
+            return true;
+        }
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (toFind.x == list[i].x && toFind.y == list[i].y)
+            {
+                if(list[i].cost <= toFind.cost)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    int CompareTwoNodes(node nodeOne, node nodeTwo)
+    {
+        if(nodeOne.heuristique < nodeTwo.heuristique)
+        {
+            return 1;
+        }
+        else if(nodeOne.heuristique == nodeTwo.heuristique)
+        {
+            return 0;
+        }
+        return -1;
     }
 
     bool IsEnoughtBlocksInMap(){
@@ -247,16 +427,28 @@ public class MapGenerator : MonoBehaviour
 	void DetermineSolidityOfInterior(){
 		int[] groundLevels = GetGroundLevels();
 		Dictionary<int, int[]> pathableCells = GetPathableCells(groundLevels);
-		Dictionary<int, int[]> underPathableCells;
+        Dictionary<int, int[]> underPathableCells;
+        Dictionary<int, int[]> overPathableCells;
 
-		for (int i = 1; i < NUMBER_OF_ROW-1; i++) {
+        for (int i = 1; i < NUMBER_OF_ROW-1; i++) {
 			if(pathableCells.ContainsKey(i)){
-                Debug.Log(i);
 				for (int j = 1; j < NUMBER_OF_COLUMN-1; j++) {
 					cellValues[i] = pathableCells[i];
 				}
 			}
 		}
+
+        overPathableCells = GetOverPathableCells(groundLevels);
+        for (int i = 1; i < NUMBER_OF_ROW - 1; i++)
+        {
+            if (overPathableCells.ContainsKey(i))
+            {
+                for (int j = 1; j < NUMBER_OF_COLUMN - 1; j++)
+                {
+                    cellValues[i] = overPathableCells[i];
+                }
+            }
+        }
 
         underPathableCells = GetUnderPathableCells(groundLevels);	
 		for (int i = 1; i < NUMBER_OF_ROW-1; i++) {
@@ -280,7 +472,95 @@ public class MapGenerator : MonoBehaviour
 		return underPathable;
 	}
 
-	int[] GetSolidityOfUnderPathable(int rowOfPathable){
+    Dictionary<int, int[]> GetOverPathableCells(int[] groundLevels)
+    {
+        Dictionary<int, int[]> overPathable = new Dictionary<int, int[]>();
+
+        foreach (int rowOfGround in groundLevels)
+        {
+            if (rowOfGround != -1)
+            {
+                int[] ground = GetSolidityOfOverPathable(rowOfGround);
+                overPathable.Add(rowOfGround-1, ground);
+                ground = GetSolidityOfOverOverPathable(rowOfGround-1, ground);
+                overPathable.Add(rowOfGround - 2, ground);
+            }
+        }
+        return overPathable;
+    }
+
+    int[] GetSolidityOfOverOverPathable(int rowOfOverPathable, int[] overGround)
+    {
+        float plain = Random.Range(0.0f,1.0f);
+        int[] ground = new int[NUMBER_OF_COLUMN];
+        if(plain >= 0.6f)
+        {
+            for(int i = 0; i < NUMBER_OF_COLUMN; i++)
+            {
+                if (overGround[i] == 1)
+                {
+                    ground[i] = 1;
+                }
+                else
+                {
+                    ground[i] = 0;
+                }
+            }
+        }
+        else
+        {
+            for(int i = 0; i < NUMBER_OF_COLUMN; i++)
+            {
+                ground[i] = 0;
+            }
+        }
+        return ground;
+    }
+
+    int[] GetSolidityOfOverPathable(int rowOfPathable)
+    {
+        int[] ground = new int[NUMBER_OF_COLUMN];
+        ground[0] = 1;
+        float probabilityOfBeeingSolid = 0.0f;
+        bool alreadyOne = false;
+        for(int i = NUMBER_OF_COLUMN-1; i > 0; i--)
+        {
+            if(cellValues[rowOfPathable][i] == 0)
+            {
+                probabilityOfBeeingSolid = 0.0f;
+            }
+            else
+            {
+                if (alreadyOne)
+                {
+                    probabilityOfBeeingSolid = 0.0f;
+                }
+                else
+                {
+                    probabilityOfBeeingSolid = 0.3f;
+                }
+                if(i < NUMBER_OF_COLUMN - 1)
+                {
+                    if(ground[i+1] == 1)
+                    {
+                        probabilityOfBeeingSolid += 0.4f;
+                    }
+                }
+                if (Random.Range(0.0f, 1.0f) + i/(NUMBER_OF_COLUMN*2) < probabilityOfBeeingSolid)
+                {
+                    ground[i] = 1;
+                    alreadyOne = true;
+                }
+                else
+                {
+                 ground[i] = 0;
+                }
+            }
+        }
+        return ground;
+    }
+
+    int[] GetSolidityOfUnderPathable(int rowOfPathable){
 		int[] ground = new int[NUMBER_OF_COLUMN];
 		ground[0] = 1;
 		float probabilityOfBeeingSolid = 0.0f;
@@ -376,7 +656,6 @@ public class MapGenerator : MonoBehaviour
 
 	int[] GetGroundLevels(){
 		int[] groundLevels = new int[maxGroundLevels];
-        Debug.Log("TEST");
 		for (int i = 0; i < maxGroundLevels; i++) {
 			groundLevels[i] = -1;
 		}
@@ -384,14 +663,10 @@ public class MapGenerator : MonoBehaviour
 		for (int i = 0; i < maxGroundLevels; i++) {
 			currentGroundLevel += Random.Range (minLevelGap+1, maxLevelGap+2);
             if (currentGroundLevel >= NUMBER_OF_ROW){
-                Debug.Log("STOP");
 				return groundLevels;
             }
-            Debug.Log("BWOP");
             groundLevels[i] = currentGroundLevel;
 		}
-
-        Debug.Log("STOPP");
         return groundLevels;
 	}
 
@@ -469,7 +744,7 @@ public class MapGenerator : MonoBehaviour
         {
             foreach (int[] coordinate in spawns)
             {
-                GameObject.Instantiate(debugTile, new Vector3(coordinate[1], -coordinate[0], 0) + transform.position, Quaternion.identity, transform);
+                //GameObject.Instantiate(debugTile, new Vector3(coordinate[1], -coordinate[0], 0) + transform.position, Quaternion.identity, transform);
             }
         }
         
